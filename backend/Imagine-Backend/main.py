5bf4b40df9248697f1805de69cd1f545a0001c0d
joshyt100 <joshyt100@gmail.com>
2024-10-08 18:59:59 -0400

    another cors change

--- backend/Imagine-Backend/main.py
+++ backend/Imagine-Backend/main.py
@@ -19,9 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://full-stack-member-management-system-1gmjknths.vercel.app/"
-    ],  # This should match your React app's URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
